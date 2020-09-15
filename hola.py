6cb54dcbd7a50168ae848ddbf3dfcafd6acc61f3
Jack <1jackiemoni@gmail.com>
2020-09-15 14:54:22 -0500

    intento de convolucion

---
+++ hola.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+img = "/home/pedros/Documents/Jackie/ulises.jpeg"
+image = cv2.imread(img)
+#image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image_copy = image.copy()
+
+#rickett = [[1,1,1],[0,0,0],[-1,-1,-1]]
+#rickett_y = np.asarray(rickett)
+#print(rickett_y)
+print(image)
+count = 0
+
+for i in range(0, image.shape[0]):
+     for j in range(0, image.shape[1]):
+          for k in range(0, image.shape[2]):
+              if count == 1:
+                  image[i][j] = [0,0,0]
+              elif count == 2:
+                  image[i][j] = image[i][j]*-1
+          count+=1
+          if count == 3:
+              count = 0
+              sum = image[i][j][0] + image[i][j][1] + image[i][j][2]
+              sum += image[i][j-1][0] + image[i][j-1][1] + image[i][j-1][2]
+              sum += image[i][j-2][0] + image[i][j-2][1] + image[i][j-2][2]
+              image[i][j-1][1] = sum
+
+while True:
+    cv2.imshow("Output", image)
+    cv2.waitKey(10)
+
+cv2.destroyAllWindows()
---
+++ hola2.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+img = "/home/pedros/Documents/Jackie/ulises.jpeg"
+image = cv2.imread(img)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image_copy = image.copy()
+
+#rickett = [[1,1,1],[0,0,0],[-1,-1,-1]]
+#rickett_y = np.asarray(rickett)
+#print(rickett_y)
+print(image)
+count = 0
+
+for i in range(0, image.shape[0]):
+     if i%3 == 2:
+          image[i] = image[i]*-1
+     elif i%3 == 1:
+          image[i] = image[i]*0
+
+for i in range(0, image.shape[0]-3, 3):
+     for j in range(0, image.shape[1]):
+          if count == 3:
+              count = 0
+              sum = image[i][j] + image[i][j-1] + image[i][j-2]
+              sum += image[i+1][j] + image[i+1][j-1] + image[i+1][j-2]
+              sum += image[i+2][j] + image[i+2][j-1] + image[i+2][j-2]
+              image_copy[i+1][j-1] = sum
+
+while True:
+    cv2.imshow("Output", image_copy)
+    cv2.waitKey(10)
+
+cv2.destroyAllWindows()
